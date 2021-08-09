b69c20e945f83c3e4ecdb0079e96607d4cc7f2b5
LT <liuting0109@126.com>
2021-08-09 17:44:13 +0800

    init-1

---
+++ src/main/java/com/psbc/sportmeeting/SportmeetingApplication.java
@@ -0,0 +1,13 @@
+package com.psbc.sportmeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportmeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportmeetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psbc/sportmeeting/SportmeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psbc.sportmeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportmeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
